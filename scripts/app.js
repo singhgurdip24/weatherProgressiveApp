9ea61ffcde47d195698fa5181922cf89876dae48
Gurdip Singh <singh.gurdip24@gmail.com>
2019-01-23 16:44:02 -0500

    Step4

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 // Copyright 2016 Google Inc.
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,18 +41,7 @@
 
   document.getElementById('butAdd').addEventListener('click', function() {
     // Open/show the add new city dialog
-    // Add the newly selected city
-   var select = document.getElementById('selectCityToAdd');
-   var selected = select.options[select.selectedIndex];
-   var key = selected.value;
-   var label = selected.textContent;
-   if (!app.selectedCities) {
-     app.selectedCities = [];
-   }
-   app.getForecast(key, label);
-   app.selectedCities.push({key: key, label: label});
-   app.saveSelectedCities();
-   app.toggleAddDialog(false);
+    app.toggleAddDialog(true);
   });
 
   document.getElementById('butAddCity').addEventListener('click', function() {
@@ -61,14 +50,9 @@
     var selected = select.options[select.selectedIndex];
     var key = selected.value;
     var label = selected.textContent;
-    if (!app.selectedCities) {
-      app.selectedCities = [];
-    }
     // TODO init the app.selectedCities array here
     app.getForecast(key, label);
     // TODO push the selected city to the array and save here
-    app.selectedCities.push({key: key, label: label});
-    app.saveSelectedCities();
     app.toggleAddDialog(false);
   });
 
@@ -197,7 +181,7 @@
         }
       } else {
         // Return the initial weather forecast since no data is available.
-        //app.updateForecastCard(initialWeatherForecast);
+        app.updateForecastCard(initialWeatherForecast);
       }
     };
     request.open('GET', url);
@@ -212,12 +196,7 @@
     });
   };
 
-  // TODO add saveSelectedCities function
-
-  app.saveSelectedCities = function() {
-    var selectedCities = JSON.stringify(app.selectedCities);
-    localStorage.selectedCities = selectedCities;
-  };
+  // TODO add saveSelectedCities function here
 
   app.getIconClass = function(weatherCode) {
     // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
@@ -327,38 +306,6 @@
   app.updateForecastCard(initialWeatherForecast);
 
   // TODO add startup code here
-
-  /************************************************************************
-     *
-     * Code required to start the app
-     *
-     * NOTE: To simplify this codelab, we've used localStorage.
-     *   localStorage is a synchronous API and has serious performance
-     *   implications. It should not be used in production applications!
-     *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
-     *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
-     ************************************************************************/
-
-    app.selectedCities = localStorage.selectedCities;
-    if (app.selectedCities) {
-      app.selectedCities = JSON.parse(app.selectedCities);
-      app.selectedCities.forEach(function(city) {
-        app.getForecast(city.key, city.label);
-      });
-    } else {
-      /* The user is using the app for the first time, or the user has not
-       * saved any cities, so show the user some fake data. A real app in this
-       * scenario could guess the user's location via IP lookup and then inject
-       * that data into the page.
-       */
-      app.updateForecastCard(initialWeatherForecast);
-      app.selectedCities = [
-        {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
-      ];
-      app.saveSelectedCities();
-    }
-
-
 
   // TODO add service worker code here
 })();
